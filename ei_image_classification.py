856f4ef47408df49f6900b216fd56b7d8c7308ec
kidswong999 <kidswong999@163.com>
2020-10-24 17:29:24 +0800

    add tflite

---
+++ ei_image_classification.py
@@ -0,0 +1,30 @@
+# Edge Impulse - OpenMV Image Classification Example
+
+import sensor, image, time, os, tf
+
+sensor.reset()                         # Reset and initialize the sensor.
+sensor.set_pixformat(sensor.RGB565)    # Set pixel format to RGB565 (or GRAYSCALE)
+sensor.set_framesize(sensor.QVGA)      # Set frame size to QVGA (320x240)
+sensor.set_windowing((240, 240))       # Set 240x240 window.
+sensor.skip_frames(time=2000)          # Let the camera adjust.
+
+net = "trained.tflite"
+labels = [line.rstrip('\n') for line in open("labels.txt")]
+
+clock = time.clock()
+while(True):
+    clock.tick()
+
+    img = sensor.snapshot()
+
+    # default settings just do one detection... change them to search the image...
+    for obj in tf.classify(net, img, min_scale=1.0, scale_mul=0.8, x_overlap=0.5, y_overlap=0.5):
+        print("**********\nPredictions at [x=%d,y=%d,w=%d,h=%d]" % obj.rect())
+        img.draw_rectangle(obj.rect())
+        # This combines the labels and confidence values into a list of tuples
+        predictions_list = list(zip(labels, obj.output()))
+
+        for i in range(len(predictions_list)):
+            print("%s = %f" % (predictions_list[i][0], predictions_list[i][1]))
+
+    print(clock.fps(), "fps")
